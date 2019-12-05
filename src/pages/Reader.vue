2f0cc7adf1ba19dc745f305cac2b3c1e2ef24811
Jakub Janik <jakubjanik@vp.pl>
2019-12-05 20:07:12 +0100

    Create Reader page component

---
+++ src/pages/Reader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>{{ src }}</div>
+</template>
+
+<script>
+export default {
+    name: 'Reader',
+    props: {
+        src: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
