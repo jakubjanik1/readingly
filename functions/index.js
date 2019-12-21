091455a425c7db54208664ab3006b7f21d4ccd5a
Jakub Janik <jakubjanik@vp.pl>
2019-12-21 11:56:23 +0100

    Set up express in Firebase functions

--- functions/index.js
+++ functions/index.js
@@ -3,62 +3,65 @@ const os = require('os')
 const gs = require('gs')
 const path = require('path')
 const fs = require('fs')
-const cors = require('cors')({ origin: true })
+const cors = require('cors')
 const admin = require('firebase-admin')
 const request = require('request')
 const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
 admin.initializeApp()
 
-exports.createThumbnail = https.onRequest((req, res) => {
-    cors(req, res, () => {
-        if (! req.body.file) {
-            return res.status(400).send('File is not specified')
-        }
+app.use(cors({ origin: true }))
 
-        let filePath = /o\/(.*)\?/.exec(req.body.file)[1].replace('%2F', '/')
+app.post('/createThumbnail', (req, res) => {
+    if (! req.body.file) {
+        return res.status(400).send('File is not specified')
+    }
 
-        const fileName = path.basename(filePath)
-        const tempFilePath = path.join(os.tmpdir(), fileName)
+    let filePath = /o\/(.*)\?/.exec(req.body.file)[1].replace('%2F', '/')
 
-        const newName = path.basename(filePath, '.pdf') + '.jpg'
-        const tempNewPath = path.join(os.tmpdir(), newName)
+    const fileName = path.basename(filePath)
+    const tempFilePath = path.join(os.tmpdir(), fileName)
 
-        const bucket = admin.storage().bucket()
+    const newName = path.basename(filePath, '.pdf') + '.jpg'
+    const tempNewPath = path.join(os.tmpdir(), newName)
 
-        return bucket.file(filePath).download({
-            destination: tempFilePath
-        }).then(() => {
-            return new Promise((resolve, reject) => {
-                gs()
-                    .batch()
-                    .nopause()
-                    .option('-dFirstPage=1')
-                    .option('-dLastPage=1')
-                    .executablePath('lambda-ghostscript/bin/./gs')
-                    .device('jpeg')
-                    .output(tempNewPath)
-                    .input(tempFilePath)
-                    .exec((err) => {
-                        if (! err) {
-                            resolve()
-                        } else {
-                            reject(err)
-                        }
-                    })
-            })
-        }).then(async () => {
-            await bucket.upload(tempNewPath, { destination: newName })
+    const bucket = admin.storage().bucket()
 
-            return res.status(200).send(`https://firebasestorage.googleapis.com/v0/b/readingly-ab5f7.appspot.com/o/${ newName }?alt=media`)
-        }).then(() => {
-            fs.unlinkSync(tempNewPath)
-            fs.unlinkSync(tempFilePath)
+    return bucket.file(filePath).download({
+        destination: tempFilePath
+    }).then(() => {
+        return new Promise((resolve, reject) => {
+            gs()
+                .batch()
+                .nopause()
+                .option('-dFirstPage=1')
+                .option('-dLastPage=1')
+                .executablePath('lambda-ghostscript/bin/./gs')
+                .device('jpeg')
+                .output(tempNewPath)
+                .input(tempFilePath)
+                .exec((err) => {
+                    if (! err) {
+                        resolve()
+                    } else {
+                        reject(err)
+                    }
+                })
         })
+    }).then(async () => {
+        await bucket.upload(tempNewPath, { destination: newName })
+
+        return res.status(200).send(`https://firebasestorage.googleapis.com/v0/b/readingly-ab5f7.appspot.com/o/${ newName }?alt=media`)
+    }).then(() => {
+        fs.unlinkSync(tempNewPath)
+        fs.unlinkSync(tempFilePath)
     })
 })
 
-exports.translate = https.onRequest((req, res) => {
-    const word = req.body.word
+app.get('/translate/:word', (req, res) => {
+    const word = req.params.word
 
     request(`https://en.bab.la/dictionary/english-polish/${ word }`, (error, {}, html) => {
         if (! error) {
@@ -72,3 +75,5 @@ exports.translate = https.onRequest((req, res) => {
         }
     })
 })
+
+exports.api = https.onRequest(app)
