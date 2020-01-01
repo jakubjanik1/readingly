40ee6c449a554f23b2a2b044a3022e1e6b173436
Jakub Janik <jakubjanik@vp.pl>
2020-01-01 13:10:30 +0100

    Add and configure vue-pdf for PdfViewer

--- src/components/reader/PdfViewer.vue
+++ src/components/reader/PdfViewer.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>{{ src }}</div>
+    <div>
+        <Pdf :src="src" :page="1" />
+        <Pdf :src="src" :page="5" />
+    </div>
 </template>
 
 <script>
+import Pdf from 'vue-pdf'
+
 export default {
     name: 'PdfViewer',
+    components: {
+        Pdf
+    },
     props: {
         src: {
             type: String,
