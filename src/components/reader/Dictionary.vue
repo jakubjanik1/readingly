bae7370a13276990e4c55b89a6259c746c5409a8
Jakub Janik <jakubjanik@vp.pl>
2020-01-07 20:24:00 +0100

    Style Dictionary component basically

--- src/components/reader/Dictionary.vue
+++ src/components/reader/Dictionary.vue
@@ -1,13 +1,17 @@
 <template>
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="isEmpty(translations)">We cannot find translations of this word</div>
-    <div v-else>
-        <div 
-            v-for="(words, key) in translations"
-            :key="key"    
-        >
-            {{ key }} <br>
-            {{ words.join(' ') }}
+    <div class="dictionary">
+        <div class="dictionary__word">{{ word }}</div>
+
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="isEmpty(translations)">We cannot find translations of this word</div>
+        <div class="dictionary__translations" v-else>
+            <div 
+                v-for="(words, key) in translations"
+                :key="key"    
+            >
+                <div class="dictionary__key">{{ key }}</div>
+                <div>{{ words.join(' ● ') }}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -39,3 +43,29 @@ export default {
     methods: { isEmpty }
 }
 </script>
+
+<style lang="scss" scoped>
+    .dictionary {
+        text-align: center;
+        max-width: 768px;
+        padding: 1em;
+
+        &__word {
+            font: {
+                weight: 600;
+                size: 1.2em;
+            }
+            color: #ff7315;
+            text-transform: uppercase;
+            padding-bottom: 1em;
+        }
+
+        &__key {
+            font-weight: 600;
+        }
+
+        &__translations > :not(:first-child) {
+            padding-top: 1em;
+        }   
+    }
+</style>
