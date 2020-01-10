fc56a53ce5eda68252b652819af5957f6aa06d44
Jakub Janik <jakubjanik@vp.pl>
2020-01-10 17:21:30 +0100

    Fix unit test of Reader page

--- tests/unit/pages/Reader.spec.js
+++ tests/unit/pages/Reader.spec.js
@@ -12,15 +12,16 @@ describe('Reader', () => {
         expect(epubViewer.props('src')).toBe(process.env.VUE_APP_FIREBASE_STORAGE_URL.replace('@', 'book.epub'))
     })
 
-    it('shows Dictionary when EpubViewer emits "text-select" event', () => {
+    it('shows Dictionary when EpubViewer emits "text-select" event', async () => {
         const wrapper = mountReader('book.epub')
         const dictionary = wrapper.find({ name: 'Dictionary' })
 
-        expect(dictionary.isVisible()).toBe(false)
+        expect(dictionary.props('word')).toBe('')
 
-        wrapper.find({ name: 'EpubViewer' }).trigger('text-select', 'mutter')
+        wrapper.find({ name: 'EpubViewer' }).vm.$emit('text-select', 'mutter')
 
-        expect(dictionary.isVisible()).toBe(true)
+        expect(wrapper.vm.$modal.show).toHaveBeenCalledWith('dictionary')
+        expect(dictionary.props('word')).toBe('mutter')
     })
 
     function mountReader(book) {
@@ -28,6 +29,9 @@ describe('Reader', () => {
             mocks: {
                 $route: {
                     params: { book }
+                },
+                $modal: {
+                    show: jest.fn()
                 }
             }
         })
