17c43b9055453e1154e3c7882b27da4e345511d8
Jakub Janik <jakubjanik@vp.pl>
2020-02-14 10:52:57 +0100

    Update unit tests

--- tests/unit/components/reader/BrightnessSlider.spec.js
+++ tests/unit/components/reader/BrightnessSlider.spec.js
@@ -1,6 +1,5 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import BrightnessSlider from '@/components/reader/BrightnessSlider'
-import Slider from 'vue-slider-component'
 import Vuex from 'vuex'
 import reader from '@/store/modules/reader'
 
@@ -27,13 +26,13 @@ describe('BrightnessSlider', () => {
     })
 
     it('sets Slider default value from store', () => {
-        const slider = wrapper.find(Slider)
+        const slider = wrapper.find({ name: 'Slider' })
 
         expect(slider.props('value')).toEqual(reader.state.brightness)
     })
 
     it('calls setBrightness mutation when Slider changes value', () => {
-        const slider = wrapper.find(Slider)
+        const slider = wrapper.find({ name: 'Slider' })
         const NEW_BRIGHTNESS = 125
 
         slider.vm.$emit('change', NEW_BRIGHTNESS)
