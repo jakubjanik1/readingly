10f501b599cadc6f002ff9040b9ba0a6413b2a8a
Jakub Janik <jakubjanik@vp.pl>
2020-02-26 18:33:09 +0100

    Create ReadingProgress component

---
+++ src/components/reader/ReadingProgress.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="reading-progress" :style="{ width: progress + '%' }"></div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'ReadingProgress',
+    computed: mapState('reader', ['progress'])
+}
+</script>
+
+<style lang="scss" scoped>
+    .reading-progress {
+        height: 4px;
+        position: fixed;
+        bottom: 0;
+        background: #000;
+    }
+</style>
---
+++ tests/unit/components/reader/ReadingProgress.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import ReadingProgress from '@/components/reader/ReadingProgress'
+import Vuex from 'vuex'
+import reader from '@/store/modules/reader'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('<ReadingProgress />', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount(ReadingProgress, {
+            localVue,
+            store: new Vuex.Store({
+                modules: {
+                    reader
+                }
+            })
+        })
+    })
+
+    it('changes progress when reader state changes', () => {
+        reader.state.progress = 25
+
+        expect(wrapper.element.style.width).toBe('25%')
+    })
+
+    it('sets default progress from store', () => {
+        expect(wrapper.element.style.width).toBe(reader.state.progress + '%')
+    })
+})
