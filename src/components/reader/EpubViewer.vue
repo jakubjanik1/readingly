4a8114a29ecf7333fe387d456ad69b9d3aab956c
Jakub Janik <jakubjanik@vp.pl>
2020-02-27 18:44:44 +0100

    Update EpubViewer

--- src/components/reader/EpubViewer.vue
+++ src/components/reader/EpubViewer.vue
@@ -4,7 +4,7 @@
 
 <script>
 import { Book } from 'epubjs'
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
     name: 'EpubViewer',
@@ -19,31 +19,43 @@ export default {
             rendition: null
         }
     },
-    mounted() {
+    async mounted() {
         const book = new Book(this.src, { openAs: 'epub' })
 
-        this.rendition = book.renderTo('epub-viewer', {
+        const rendition = book.renderTo('epub-viewer', {
             manager: 'continuous',
             flow: 'scrolled',
             width: '100%',
             height: '100%'
         })
 
-        this.rendition.themes.register('white', { body: { background: '#fff', color: '#333' }})
-        this.rendition.themes.register('black', { body: { background: '#333', color: '#e7e7e7' }})
-        this.rendition.themes.register('sepia', { body: { background: '#bfb79d', color: '#e7e7e7' }})
+        this.rendition = rendition
 
-        this.rendition.themes.fontSize(this.fontSize + '%')
-        this.rendition.themes.select(this.theme)
-        this.rendition.display()
+        this.setProgress(0)
+        rendition.themes.register('white', { body: { background: '#fff', color: '#333' }})
+        rendition.themes.register('black', { body: { background: '#333', color: '#e7e7e7' }})
+        rendition.themes.register('sepia', { body: { background: '#bfb79d', color: '#e7e7e7' }})
+        rendition.themes.fontSize(this.fontSize + '%')
+        rendition.themes.select(this.theme)
+        
+        rendition.display()
 
-        this.rendition.on('selected', async cfiRange => {
+        rendition.on('selected', async cfiRange => {
             const range = await book.getRange(cfiRange)
             
             this.$emit('text-select', range.toString())
         })
+
+        await book.ready
+        book.locations.generate(1000)
+
+        rendition.on('relocated', (location) => {
+            const progress = book.locations.percentageFromCfi(location.start.cfi)
+            this.setProgress(progress * 100)
+        })
     },
-    computed: mapState('reader', ['fontSize', 'theme']),
+    computed: mapState('reader', ['fontSize', 'theme', 'progress']),
+    methods: mapMutations('reader', ['setProgress']),
     watch: {
         fontSize(newFontSize) {
             this.rendition.themes.fontSize(newFontSize + '%')
