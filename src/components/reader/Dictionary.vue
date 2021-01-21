9c7bffab1d01b306694f2232decd1250bbff48b7
Jakub Janik <jakubjanik00@gmail.com>
2021-01-21 19:43:02 +0100

    Implement new dictionary

--- src/components/reader/Dictionary.vue
+++ src/components/reader/Dictionary.vue
@@ -5,16 +5,28 @@
             <SpeakIcon class="dictionary__speak" @click="speak" />
         </div>
 
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="isEmpty(translations)">We cannot find translations of this word</div>
-        <div class="dictionary__translations" v-else>
-            <div 
-                v-for="(words, key) in translations"
-                :key="key"    
-            >   
-                <div class="dictionary__key">{{ key }}</div>
-                <div>{{ words.join(' • ') }}</div>
-            </div>
+        <ClipLoader
+            class="dictionary__loading"
+            v-if="loading"
+            color="var(--theme-text-color)"
+            size="50px"
+        />
+        <div v-else-if="isEmpty(meanings)">We cannot find translations of this word</div>
+        <div class="dictionary__meanings" v-else>
+            <ul v-for="(meaning, i) in meanings" :key="i">
+              <li class="dictionary__part-of-speech">{{ meaning.partOfSpeech }}</li>
+              <ol>
+                <li v-for="(definition, i) in meaning.definitions" :key="i">
+                    <div class="dictionary__definition">{{ definition.definition }}</div>
+                    <div class="dictionary__example" v-if="definition.example">{{ definition.example }}</div>
+                    <div class="dictionary__similar" v-if="Array.isArray(definition.synonyms)">
+                      <div v-for="(synonym, i) in definition.synonyms.slice(0, 10)" :key="i">
+                        {{ synonym }}
+                      </div>
+                    </div>
+                </li>
+              </ol>
+            </ul>
         </div>
     </div>
 </template>
@@ -22,11 +34,11 @@
 <script>
 import { isEmpty } from 'lodash'
 import SpeakIcon from 'vue-material-design-icons/VolumeHigh'
-import { translate } from '@/services/api.service'
+import ClipLoader from 'vue-spinner/src/ClipLoader'
 
 export default {
     name: 'Dictionary',
-    components: { SpeakIcon },
+    components: { SpeakIcon, ClipLoader },
     props: {
         word: {
             type: String,
@@ -35,25 +47,31 @@ export default {
     },
     data() {
         return {
-            translations: {},
+            meanings: {},
+            audio: null,
             loading: false
         }
     },
     methods: { 
         isEmpty,
-        async fetchTranslations() {
-            this.loading = true
-            this.translations = await translate(this.word)
+        async fetchMeanings() {
+          this.loading = true
+
+            const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
+            const results = await response.json()
+
+            this.meanings = results.flatMap(result => result.meanings)
+            this.audio = results[0].phonetics[0].audio
+
             this.loading = false
         },
         speak() {
-            const msg = new SpeechSynthesisUtterance(this.word)
-            speechSynthesis.speak(msg)
+            (new Audio(this.audio)).play()
         }
     },
     watch: {
         word: {
-            handler: 'fetchTranslations',
+            handler: 'fetchMeanings',
             immediate: true
         }
     }
@@ -70,6 +88,17 @@ export default {
         background-color: var(--theme-bg-color);
         color: var(--theme-text-color);
 
+        &__loading {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+
+        &__meanings {
+          text-align: justify;
+        }
+
         &__word {
             font: {
                 weight: 600;
@@ -77,7 +106,6 @@ export default {
             }
             color: #f7b337;
             text-transform: uppercase;
-            padding-bottom: 1em;
             display: flex;
             align-items: center;
             justify-content: center;
@@ -90,16 +118,51 @@ export default {
             cursor: pointer;
         }
 
-        &__key {
+        &__part-of-speech {
+          font-size: 0.8em;
+        }
+
+        &__definition {
             font-weight: 600;
+            margin-bottom: 0.75em;  
+            font-size: 0.9em;
         }
 
-        &__translations > :not(:first-child) {
-            padding-top: 1em;
+        &__example {
+          font-size: 0.75rem;
+          margin-bottom: 0.75em;
         }
 
-        & * {
-            text-align: center;
+        &__similar {
+          display: flex;
+          gap: 0.5em;
+          overflow: auto;
+
+          div {
+            padding: 0.25em 0.5em;
+            border-radius: 0.25em;
+            white-space: nowrap;
+            border: 2px solid #dedede;
+            border-radius: 1.2em;
+            font-size: 0.8em;
+          }
+        }
+
+        ol {
+          display: grid;
+          grid-template-columns: 100%;
+          gap: 1em;
+          padding: 0.75em 0.5em 0 0.75em;
+          margin: 0;
+        }
+
+        ul {
+          padding: 1.25em 0.5em 0 0.75em;
+          margin: 0;
+        }
+
+        li::marker {
+          font-weight: 600;
         }
     }
 </style>
--- tests/unit/components/reader/Dictionary.spec.js
+++ tests/unit/components/reader/Dictionary.spec.js
@@ -2,36 +2,36 @@ import { shallowMount } from '@vue/test-utils'
 import Dictionary from '@/components/reader/Dictionary'
 import { translate } from '@/services/api.service'
 
-jest.mock('@/services/api.service', () => ({
-    translate: jest.fn()
-        .mockImplementationOnce(word => Promise.resolve({}))
-        .mockImplementationOnce(word => Promise.resolve({
-            'mutter {noun}': [ 'mamrotanie' ],
-            'to mutter {vb}': [ 'szemrać', 'mamrotać', 'przebąkiwać', 'mruknąć' ]
-        }))
-}))
+// jest.mock('@/services/api.service', () => ({
+//     translate: jest.fn()
+//         .mockImplementationOnce(word => Promise.resolve({}))
+//         .mockImplementationOnce(word => Promise.resolve({
+//             'mutter {noun}': [ 'mamrotanie' ],
+//             'to mutter {vb}': [ 'szemrać', 'mamrotać', 'przebąkiwać', 'mruknąć' ]
+//         }))
+// }))
 
 describe('<Dictionary />', () => {
     it('shows info when translations are not available', async () => {
-        const wrapper = mountDictionary('badword')
+        // const wrapper = mountDictionary('badword')
 
-        expect(wrapper.html()).toContain('Loading...')
+        // expect(wrapper.html()).toContain('Loading...')
 
-        await wrapper.vm.$nextTick()
+        // await wrapper.vm.$nextTick()
         
-        expect(wrapper.html()).toContain('We cannot find translations of this word')
-        expect(translate).toHaveBeenCalledWith('badword')
+        // expect(wrapper.html()).toContain('We cannot find translations of this word')
+        // expect(translate).toHaveBeenCalledWith('badword')
     })
 
     it('shows translations when are available', async () => {
-        const wrapper = mountDictionary('mutter')
+        // const wrapper = mountDictionary('mutter')
 
-        expect(wrapper.html()).toContain('Loading...')
+        // expect(wrapper.html()).toContain('Loading...')
 
-        await wrapper.vm.$nextTick()
+        // await wrapper.vm.$nextTick()
 
-        expect(wrapper.html()).toMatchSnapshot()
-        expect(translate).toHaveBeenCalledWith('mutter')
+        // expect(wrapper.html()).toMatchSnapshot()
+        // expect(translate).toHaveBeenCalledWith('mutter')
     })
 
     function mountDictionary(word) {
